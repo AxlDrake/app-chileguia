5ddceb6801a003e0e1a27ff72b409aa0a001c3ca
AxlDrake <15652231+AxlDrake@users.noreply.github.com>
2021-06-16 20:11:16 -0400

    Version 1.0.3 play store

--- app/src/main/java/com/brouter/app_chileguia/Splash_Screen.kt
+++ app/src/main/java/com/brouter/app_chileguia/Splash_Screen.kt
@@ -4,16 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.brouter.app_chileguia.databinding.ActivitySplashScreenBinding
 
 @Suppress("DEPRECATION")
 class Splash_Screen : AppCompatActivity() {
 
     private val SPLASH_TIME : Long = 3000
+    private val version = BuildConfig.VERSION_NAME
+    private lateinit var binding:ActivitySplashScreenBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txtVersion.text = "v$version"
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
